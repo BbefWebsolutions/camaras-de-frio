04737d189192b561a2e085642340f143b0fa85fb
BbefWebsolutions <bbef.websolutions@gmail.com>
2023-03-21 11:33:56 -0300

    cambio de bd a postgres

--- appConfig/appConfig/settings.py
+++ appConfig/appConfig/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'appConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASS"),
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
+
 
 
 # Password validation
